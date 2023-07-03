8e583e5fec752a96ff3c885fee3dae9b8dc332c2
RomanBAchu <116597198+RomanBAchu@users.noreply.github.com>
2023-07-03 21:58:04 +0300

    Update front.py

--- front.py
+++ front.py
@@ -1,9 +1,10 @@
 import vk_api
-import db
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.utils import get_random_id
-from config import community_token, access_token
+
+import db
 from back import VkTools
+from config import community_token, access_token
 from keyboard import *
 
 
@@ -15,7 +16,8 @@ class Bot_interface():
         self.params = {}
         self.worksheets = []
         self.offset = 0
-        print('VKinder готов!', sep='\n')
+
+        print(' \n V\n K\n i\n n       готов!\n d\n e\n r\n', sep='\n')
 
     def message_send(self, user_id, message, attachment=None):
         self.vk.method('messages.send',
@@ -35,7 +37,7 @@ class Bot_interface():
 
     def find_city(self, user_id):
         # Функция получения города в случае его отсутствия в профиле
-        self.message_send(user_id, 'Введите название города для поиска: ')
+        self.message_send(user_id, 'Введи название города для поиска: ')
         message, user_id = self.event_handler()
         hometown = str(message)
         cities = self.vk_tools.get_cities(user_id)
@@ -50,7 +52,7 @@ class Bot_interface():
     def event_handler(self):
         for event in self.longpoll.listen():
             if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-                if event.text.lower() == 'привет':
+                if event.text.lower() == 'привет' or event.text.lower() == 'поиск':
                     '''Логика для получения данных о пользователе'''
                     self.params = self.vk_tools.get_profile_info(event.user_id)
                     # Запрос доп. информации, в случае если она отсутствует в профиле
@@ -60,54 +62,130 @@ class Bot_interface():
                                            f'Для продолжения работы с ботом укажи свой город')
                         event.to_me = self.find_city(event.user_id)
                     else:
-                        self.message_send(event.user_id, f'Здарова, {self.params["name"]}! &#128587;&#8205;&#9794;&#65039;')
-                elif event.text.lower() == 'поиск':
-                    '''Логика для поиска анкет'''
-                    if self.worksheets:
-                        worksheet = self.worksheets.pop()
-                        photo_string = self.get_user_photo(worksheet['id'])
-                    else:
-                        self.worksheets = self.vk_tools.search_worksheet(
-                            self.params, self.offset)
 
-                        worksheet = self.worksheets.pop()
-                        'проверка анкеты в бд в соответствие с event.user_id'
+                        if event.text.lower() == 'привет':
+                            name_parts = self.params["name"].split()
+                            first_name = name_parts[0]
 
-                        connection = db.connect_to_database()
-                        db.create_table_seen_users(connection)
-                        num_users_shown = 0  # Количество показанных пользователей
-                        shown_users = set()  # Множество показанных пользователей
+                            self.message_send(event.user_id, f'Здарова, {first_name}! &#128587;&#8205;&#9794;&#65039;')
 
-                        while worksheet and num_users_shown < 1:  # Показывать только 1 пользователя
-                            # worksheet = worksheet.pop()
-                            if str(worksheet['id']) not in shown_users:
-                                shown_users.add(str(worksheet['id']))
-                                num_users_shown += 1
-                                # Проверка наличия пользователя в БД
-                                if db.check_seen_user(connection, str(worksheet['id'])):
-                                    self.message_send(event.user_id, f"{worksheet['name']} уже есть в базе данных.")
-                                    continue
 
-                        photo_string = self.get_user_photo(worksheet['id'])
-                        self.offset += 50
 
-                    self.message_send(event.user_id,
-                                      f'Посмотри, это - {worksheet["name"]}',
-                                      attachment=photo_string)
-                    self.message_send(event.user_id,
-                                      f'Вот страница VK: https://vk.com/id{worksheet["id"]}')
 
-                    'добавить анкету в бд в соответствие с event.user_id'
+
+                        elif event.text.lower() == 'поиск':
+
+                            photo_string = None  # Значение по умолчанию
+
+                            if self.worksheets:
+
+                                try:
+                                    worksheet = self.worksheets.pop()
+
+                                    photo_string = self.get_user_photo(worksheet['id'])
+
+
+
+                                except IndexError:
+
+                                    self.worksheets = self.vk_tools.search_worksheet(self.params, self.offset)
+
+                                    worksheet = self.worksheets.pop()
+
+                            else:
+
+                                self.worksheets = self.vk_tools.search_worksheet(self.params, self.offset)
+
+                                if len(self.worksheets) > 0:
+
+                                    worksheet = self.worksheets.pop()
+
+                                else:
+
+                                    # обработка случая, когда список пустой
+
+                                    worksheet = None  # или выполните нужные действия вместо использования метода pop()
+
+                            connection = db.connect_to_database()
+
+                            db.create_table_seen_users(connection)
+
+                            num_users_shown = 0  # Количество показанных пользователей
+
+                            shown_users = set()  # Множество показанных пользователей
+
+                            print(f"Показано {num_users_shown} анкет")
+
+                            # Выполнение кода
+
+                            # worksheet = None
+                            num_users_shown = 0
+
+                            while num_users_shown < 1:
+                                if 'worksheet' in locals() or 'worksheet' in globals():
+                                    if worksheet is not None:
+                                        # Преобразование словаря в кортеж
+                                        worksheet_tuple = tuple(worksheet.items())
+                                        if worksheet_tuple not in shown_users:
+                                            shown_users.add(worksheet_tuple)
+                                            num_users_shown += 1
+
+                                            # Проверка наличия пользователя в БД
+                                            if db.check_seen_user(connection, str(worksheet['id'])):
+                                                self.message_send(event.user_id,
+                                                                  f"{worksheet['name']}\n\n Мы с тобой уже видели эту анкету. &#128522;")
+                                                continue
+
+
+
+                                self.offset += 50
+
+                                if 'worksheet' in locals() or 'worksheet' in globals():
+                                    if worksheet is not None:
+                                        self.message_send(event.user_id,
+                                                          f'Посмотри, это - {worksheet["name"]}.\nВот страница VK: https://vk.com/id{worksheet["id"]}',
+                                                          attachment=photo_string)
+
+
+
+
+                                        connection = db.connect_to_database()
+                                        db.insert_data_seen_users(connection, str(worksheet["id"]), 0)
+
+                                # Добавить анкету в бд в соответствие с event.user_id
+                                connection = db.connect_to_database()
+                                if 'worksheet' in locals() or 'worksheet' in globals():
+                                    if worksheet is not None and "id" in worksheet:
+                                        db.insert_data_seen_users(connection, str(worksheet["id"]), 0)
+
+                                        print(f"Новая {num_users_shown} анкет")
+
+
+
+                elif event.text.lower() == 'выкл':
+
+                    self.message_send(event.user_id, 'ВЫКЛЮЧАЮ ПРОГУ')
+
+                    # Очистка сохраненных данных (если они есть)
+
                     connection = db.connect_to_database()
-                    db.insert_data_seen_users(connection, str(worksheet["id"]), 0)
 
-                elif event.text.lower() == 'пока':
-                    self.message_send(event.user_id, 'Ну пока')
-                    connection = db.connect_to_database()
                     db.remove_table_seen_users(connection)
+
                     db.disconnect_from_database(connection)
+
+
+
+
+
+
+
+
                 else:
-                    self.message_send(event.user_id, 'Команда не распознана')
+                    self.message_send(event.user_id, 'Если хочешь, что бы я находил тебе '
+                                                     'подходящих людей, то поздоровайся со мной, а потом ищи!\n\n Если '
+                                                     'сразу будешь искать, то я буду находить кого попало.\n\n Так же можешь '
+                                                     'нажимать на мои кнопочки. &#128077;')
 
 
 if __name__ == '__main__':
