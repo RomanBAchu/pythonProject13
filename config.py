5977c3d927285faf00d0f4f752e97a00a47f1b1e
RomanBAchu <116597198+RomanBAchu@users.noreply.github.com>
2023-07-03 16:07:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@
 """         группы, фронтэндовый, даётся на месяц. """
 #                    Копируем и вводим внутрь апострофов, то-есть между: ' ' .
 
-community_token = 'vk1.a.FdZjRQAfF307eywF7DJ6K1Fy1yPN4A982rbKtklPH7hGXci11reRq9xk-0cJS-aaLSrXtRcmlvZ5sV60-WSjOnKDLJNFzeO4mzuDrZuUKqCk67YXSqX3ATdS5Pg02eOzZEZGmC-WEhRFDtNF76sDBMD7rvoutAF-JmHn8grl-FhaOZEFgsw2Q82zUJ9kur2mw5rzQkQe4AH6vbJttiyEdw'
+community_token = '  '
 
 
 #                 Токен доступа для работы с API ВКонтакте,
 """         токен приложения, ну или типо бэкэнд личной странцицы, даётся на сутки. """
 #                    Копируем и вводим внутрь апострофов, то-есть между: ' ' .
 
-access_token = 'vk1.a.bbnw643jtGtIdYWG8HnJqZg2odPhhVfI-bJrwuOPl_1W7Lb6u_UeaKWy3RxUvPW3hprYSIRYO_rABGnE6vZORsHahcKpnmJzjEcxHqSt5sL1YLq1uSB34as9sL2w83sH0bNj3oioBFLb75PGVmoYNsTrOaYBHnnPmLm8N-oQpw131q7RmbVSaM9LU798m7qg6uhoGGh4D7tDzLZojvFiJQ'
+access_token = ' '
 
 
 
